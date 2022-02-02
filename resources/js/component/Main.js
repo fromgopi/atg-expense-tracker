bbfb987967ec228d273071f3212b6e78263a64f3
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-02-02 11:59:17 +0530

    setting some react and react-dom
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

---
+++ resources/js/component/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main = () => {
+    return(
+        <div>Welcome to ATG Expense Tracker</div>
+    )
+}
+
+export default Main;
---
+++ resources/js/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom';
+import Main from './component/Main';
+
+if(document.getElementById("atg-et")){
+    ReactDOM.render(<Main />, document.getElementById("atg-et"))
+}
