60b1b471c0c59bbd6596cd7c182fe3c92424aa1b
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-02-14 23:25:02 +0530

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
     /*
     |-------------------------------------------------------------------------- 
     | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
+    |-------------------------------------------------------------------------- 
     |
     | Here you may configure your settings for cross-origin resource sharing
     | or "CORS". This determines what cross-origin operations may execute
