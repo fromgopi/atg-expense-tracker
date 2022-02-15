a493bd5b1d500e1fd98c769f8a9131044e9e3eb8
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-02-15 23:02:46 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------  
 | Web Routes
-|--------------------------------------------------------------------------
+|-------------------------------------------------------------------------- 
 |
 | Here is where you can register web routes for your application. These  
 | routes are loaded by the RouteServiceProvider within a group which 
