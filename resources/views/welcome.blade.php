93104a3e93df414f0eb482f9884b072377aa69de
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-02-16 21:14:45 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <title>ATG-ET</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" 
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"  
             rel="stylesheet"
         >
         <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
