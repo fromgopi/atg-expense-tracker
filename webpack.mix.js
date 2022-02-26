b1a1de44110a29c133fd09b8eef056f3ecabe4a9
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-02-26 21:44:01 +0530

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix'); 
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 /*
 |--------------------------------------------------------------------------
