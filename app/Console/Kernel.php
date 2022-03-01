377e757989d4ac4fce84a595048987debf2ca66d
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-03-01 22:12:28 +0530

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,7 +11,7 @@ class Kernel extends ConsoleKernel
      * Define the application's command schedule.  
      * 
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
+     * @return void 
      */
     protected function schedule(Schedule $schedule)
     {
