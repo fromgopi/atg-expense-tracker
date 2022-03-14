47c6c3fdd1b6cb61aa96a37dc539edf96947783e
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-03-14 20:27:24 +0530

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,7 @@ const mix = require('laravel-mix');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 /*
 |--------------------------------------------------------------------------
-| Mix Asset Management
+| Mix Asset Management 
 |--------------------------------------------------------------------------  
 | 
 | Mix provides a clean, fluent API for defining some Webpack build steps  
