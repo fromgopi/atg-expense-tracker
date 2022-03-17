aabf7f7a8793234af959c25c86789ffe790873d2
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-03-17 21:00:18 +0530

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,7 +6,7 @@ const ImageminPlugin = require('imagemin-webpack-plugin').default;
 |--------------------------------------------------------------------------  
 |   
 | Mix provides a clean, fluent API for defining some Webpack build steps   
-| for your Laravel applications. By default, we are compiling the CSS   
+| for your Laravel applications. By default, we are compiling the CSS    
 | file for the application as well as bundling up all the JS files.    
 |
  */
